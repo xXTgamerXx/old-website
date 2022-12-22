5876f2d6f28d4bf3954fb370e4f3ee734a4f7d19
Trinethr N <trinethr_n@protonmail.com>
2022-12-22 23:38:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# [brin.is-a.dev/old-website](https://brin.is-a.dev/old-website)
+Personal website :) (**OLD**)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}", "./index.html"],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                'nunito': ['"Nunito"', 'sans-serif'],
+                'poppins': ['"Poppins"', 'sans-serif'],
+                'comfortaa': ['"Comfortaa"', 'cursive'],
+            }
+        },
+    },
+    plugins: [],
+}
